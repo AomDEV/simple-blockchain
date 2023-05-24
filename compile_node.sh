6193ff8101b892e774ff87166f1f3cb2cd643e37
Aom Siriwat <siriwat576@gmail.com>
2023-05-25 02:08:16 +0700

    build: add debugging flags to client and node compilation scripts
    The client and node compilation scripts have been updated to include the -g debugging flag. This enables easier debugging of any issues encountered during execution.

--- compile_node.sh
+++ compile_node.sh
@@ -5,5 +5,5 @@ gcc -c -o dict.o dict.c
 gcc -c -o blockchain.o blockchain.c
 gcc -I /opt/homebrew/include -L/opt/homebrew/lib/ \
 node.c blockchain.o util.o httpd.o dict.o \
--o node \
+-o node -g \
 -lcrypto -lssl -lpthread -Wall -fcommon -lm
