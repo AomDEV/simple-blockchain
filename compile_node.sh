f2af56cbb983fac47da19373b1f9b9619327dd51
Aom Siriwat <siriwat576@gmail.com>
2023-05-24 22:08:36 +0700

    feat: Add dictionary functionality and compile_node.sh updated to include dictionary.o
    This commit adds a complete dictionary implementation, including functions for creating the dictionary, inserting and deleting key-value pairs, accessing values, as well as freeing the dictionary's memory. Additionally, the compile script has been updated to compile dictionary.c into a dictionary.o object file for linking with the node executable.

--- compile_node.sh
+++ compile_node.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 gcc -c -o httpd.o httpd.c
 gcc -c -o util.o util.c
+gcc -c -o dict.o dict.c
 gcc -c -o blockchain.o blockchain.c
 gcc -I /opt/homebrew/include -L/opt/homebrew/lib/ \
-node.c blockchain.o util.o httpd.o \
+node.c blockchain.o util.o httpd.o dict.o \
 -o node \
 -lcrypto -lssl -lpthread -Wall -fcommon -lm
